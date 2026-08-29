93f65f272d73cf8ddaf2818c1513ee22e83941c6
suzuki haruya <doprogramtopic@outlook.jp>
2026-08-29 11:40:00 +0900

    build: CMake によるクロスプラットフォームビルドに対応
    Visual Studio 以外の環境でもビルドできるよう CMakeLists.txt を追加する。
    Windows SDK 依存の targetver.h と tchar.h を _MSC_VER でガードし、
    glibc の math.h と衝突する M_PI の定義も条件付きにした。
    MSVC 側には /utf-8 を渡し、BOM なしの UTF-8 ソースを正しく読ませる。

--- DDPG/DDPG/stdafx.h
+++ DDPG/DDPG/stdafx.h
@@ -5,10 +5,15 @@
 
 #pragma once
 
+// targetver.h と tchar.h は Windows SDK 依存のため、MSVC 以外では取り込まない
+#ifdef _MSC_VER
 #include "targetver.h"
+#endif
 
 #include <stdio.h>
+#ifdef _MSC_VER
 #include <tchar.h>
+#endif
 
 
 
--- DDPG/DDPG/unit.h
+++ DDPG/DDPG/unit.h
@@ -4,7 +4,9 @@
 #include <stdlib.h>
 #include "string.h"
 
+#ifndef M_PI
 #define M_PI 3.14159
+#endif
 
 
 typedef struct {
